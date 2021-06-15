42fd9c1ee63e5f6033821f4ea9c2e3fc7eae227b
clvrisa <77904575+clvrisa@users.noreply.github.com>
2021-06-14 21:28:25 -0700

    Exercise8

---
+++ Exercise8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise8
+{
+    class A
+    {
+        int x;
+        public abstract void f(int n);
+        public virtual void g(uint n)
+        {
+            x = (int)n;
+        }
+        public string ToString()
+        {
+            return x.ToString();
+        }
+    }
+}
