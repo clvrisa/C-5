9fd4ab17ea513a8da16c8c03faafa5d8261a9010
clvrisa <77904575+clvrisa@users.noreply.github.com>
2021-06-14 21:25:28 -0700

    Exercise2

---
+++ Exercise2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace C5
+{
+    class A
+    {
+        protected int n = 1;
+        public A()
+        {
+            n++;
+            Console.WriteLine("n="+n.ToString());
+        }
+    }
+    class B : A
+    {
+        public B()
+        {
+            n = 10;
+            Console.WriteLine("n="+n.ToString());
+        }
+    }
+    class MainClass
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            A a = new B();
+        }
+    }
+}
