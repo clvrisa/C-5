ddad8313b0d45ddf96b6a6b61fc40c529a064766
clvrisa <77904575+clvrisa@users.noreply.github.com>
2021-06-14 21:25:12 -0700

    Exercise1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using.System.text;
+
+
+namespace C5
+{
+    class Square
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a number =>");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if(a>0 && a<=10)
+            {
+                a = a << 1;
+                Console.Write("{0},", a);
+            }
+            Console.ReadKey();
+        }
+    }
+}
