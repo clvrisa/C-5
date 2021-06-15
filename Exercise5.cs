aee0e3209218c87a5d63ce43af4e2b31b8c4b92f
clvrisa <77904575+clvrisa@users.noreply.github.com>
2021-06-14 21:26:33 -0700

    Exercise5

---
+++ Exercise5.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace C5
+{
+    public class Exercise5
+    {
+        public delegate void LogHandler(string message);
+        public void process(LogHandler logHandler)
+        {
+            if (logHandler != null)
+            {
+                logHandler("begin");
+            }
+            if (logHandler != null)
+            {
+                logHandler("End");
+            }
+        }
+        public class MyLogger
+        {
+            public void logger(string s)
+            {
+                Console.WriteLine(s);
+            }
+        }
+        public class TestApplication
+        {
+            static void Logger(string s)
+            {
+                Console.WriteLine(s);
+            }
+        }
+        static void Main (string[] args)
+        {
+            MyLogger f1 = new MyLogger();
+            Exercise5 exercise5 = new Exercise5();
+            Exercise5.LogHandler myLogger = null;
+            MyLogger += new Exercise5.LogHandler(myLogger);
+            MyLogger += new Exercise5.LogHandler(f1.logger);
+            Exercise5.process(ref myLogger);
+            return;
+        }
+    }
+}
