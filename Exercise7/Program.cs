fd5e29514e680db0104f8103033e8b17ed99dbf7
clvrisa <77904575+clvrisa@users.noreply.github.com>
2021-06-14 21:28:08 -0700

    Exercise7

---
+++ Exercise7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = 0;
+            Console.WriteLine("Please enter number:");
+            number = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Rounded calue of {0} is {1}", number, (int) (number + 0.5));
+            Console.ReadKey();
+        }
+    }
+}
