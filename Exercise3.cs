a6594c9970620538f58b0e77be21533bd790e711
clvrisa <77904575+clvrisa@users.noreply.github.com>
2021-06-14 21:26:03 -0700

    Exercise3

---
+++ Exercise3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace C5
+{
+    class Exercise3
+    {
+        static void f(string s)
+        {
+            s += "world";
+        }
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string s = "Hello";
+            f(s);
+            Console.WriteLine(s);
+        }
+    }
+}
